6a27c45468cf66f0e68500dd7420192e3daef9da
ademhilmibozkurt <bozkurtadem7@gmail.com>
2025-12-23 22:24:00 +0300

    feature/AppResponses
    Desktop application now takes responses from database.

--- desktop/app.py
+++ desktop/app.py
@@ -1,27 +1,37 @@
-import requests
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton
 import sys
-
-API_URL = "https://YOUR_DOMAIN"
+from database import supabase
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton
 
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Studio Panel")
+        self.setWindowTitle("Foto Model")
 
         layout = QVBoxLayout()
         self.log = QTextEdit()
 
-        btn = QPushButton("Yeni Yanıtları Getir")
+        # make these prettier
+        btn = QPushButton("Güncelle")
         btn.clicked.connect(self.fetch_data)
 
         layout.addWidget(btn)
         layout.addWidget(self.log)
         self.setLayout(layout)
 
+    # add a list of customers
+    # select one customer
+    # show selected photo templates
+    # add is_made button and allow for deletion
     def fetch_data(self):
-        r = requests.get(f"{API_URL}/responses")
-        self.log.setText(str(r.json()))
+        responses = (
+            supabase
+            .table("responses")
+            .select("*")
+            .order("created_at", desc=True)
+            .execute()
+        )
+
+        self.log.setText(str(responses))
 
 app = QApplication(sys.argv)
 window = App()
